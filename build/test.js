89ea98b9e73ce16ade7fd87c31eedaaf2cddc720
Lightman <yaruk.my@gmail.com>
2015-09-16 18:38:28 +0300

    Write test for array

--- build/test.js
+++ build/test.js
@@ -14,3 +14,25 @@ QUnit.test('#has', function() {
 	ok(h.obj.has({a:1}, 'a'));
 	ok(!h.obj.has({}, 'a'));
 });
+
+QUnit.module('Array');
+QUnit.test('#is', function() {
+	ok(h.arr.is([]));
+	ok(!h.arr.is(''));
+	ok(!h.arr.is(null));
+	ok(!h.arr.is(undefined));
+	ok(!h.arr.is(3));
+	ok(!h.arr.is(function() {}));
+});
+QUnit.test('#remove', function() {
+	var a = [1, '3', 0];
+	h.arr.remove(a, 1);
+	ok(a.indexOf(1) == -1);
+
+	h.arr.remove(a, 0);
+	h.arr.remove(a, 'some'); // ensure that array is not modified when element does not exist
+	ok(a.indexOf(0) == -1);
+
+	h.arr.remove(a, '3');
+	ok(a.indexOf('3') == -1 && a.length === 0);
+});
--- src/array.js
+++ src/array.js
@@ -1,10 +1,10 @@
 module.exports = {
 	is: function (possibleArr) {
-		return (!!a) && (a.constructor === Array);
+		return (!!possibleArr) && (possibleArr.constructor === Array);
 	},
-	remove: function(array, elVal) {
-		var index = array.indexOf(elVal);
-		if (index != -1) array.splice(index, 1);
+	remove: function(array, element) {
+		var i = array.indexOf(element);
+		if (i != -1) array.splice(i, 1);
 		return array;
 	}
 };

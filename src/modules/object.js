4088554ff7b9436a27b70b0e9977200579dbfce4
Lightman <yaruk.my@gmail.com>
2017-08-13 11:54:10 +0300

    add .get

--- src/modules/object.js
+++ src/modules/object.js
@@ -7,5 +7,15 @@ module.exports = {
 	},
 	has: function(obj, property) {
 		return obj.hasOwnProperty(property);
-	}
+	},
+	get: function(obj, key) {
+		if(typeof obj === 'undefined') return false;
+		var index = prop.indexOf('.');
+
+		if(index > -1) {
+			return this.get(obj[prop.substring(0, index)], prop.substr(index + 1));
+		}
+		
+		return obj[prop];
+	},
 };
